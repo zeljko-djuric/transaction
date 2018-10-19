c672c6d50acfafba420e6787a768622a5babacce
Zeljko Djuric <zeljko94@gmail.com>
2018-10-19 14:27:49 +0200

    Added MainCompoenent

---
+++ src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class MainContent extends React.Component{
+
+    render(){
+
+        return(
+            <h1> Hello from main content </h1>
+        )
+    }
+}
+
+export default MainContent;
