1848dcd51e7e87562aea95f7a29ad88d3fb8aa71
Zeljko Djuric <zeljko94@gmail.com>
2018-10-19 15:25:41 +0200

    Added Input and button

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,28 @@
 import React from 'react';
-
+import { Input, Row, Col, Button } from 'antd';
+import 'antd/dist/antd.css';
 
 class MainContent extends React.Component{
+
+    handleClick(){
+        console.log('you clicked');
+    }
 
     render(){
 
         return(
-            <h1> Hello from main content </h1>
+            <div>
+            <h1> Transaction</h1>
+            <Row>
+                <Col span={8}>
+                    <Input placeholder="Value"/>
+                </Col>
+                <Col span={8}>
+                    <Button type="primary" onClick={this.handleClick}>Send</Button>
+                </Col>
+            </Row>
+            
+            </div>
         )
     }
 }
