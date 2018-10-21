5407ae4f46dec0d41fadf264f4df15b715746b61
Zeljko Djuric <zeljko94@gmail.com>
2018-10-21 13:52:57 +0200

    Added feature to send money on adress that user entered

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,21 +5,33 @@ import Web3 from 'web3';
 
 class MainContent extends React.Component{
 
-    handleClick = () => {
-            let web3js = new Web3(window.web3.currentProvider); 
-            web3js.eth.sendTransaction({
-                to: '0x47ef3D23598Dff40e16ec55aa02B9f026124720F',
-                from: '0x1dC898665B762C781Ff6110dA46B83EE6Cb5C791',
-                value: web3js.utils.toWei('1', 'ether'), 
-            })      
-    } 
+    constructor(props){
+        super(props);
+        this.state={
+            adress: ''
+        }
+    }
+
+    updateInput = (event) =>{
+        this.setState({adress : event.target.value})
+    }
+
+
+    handleClick = () =>{
+        let web3js = new Web3(window.web3.currentProvider);
+        web3js.eth.sendTransaction({
+            to: this.state.adress,
+            from: '0x1dC898665B762C781Ff6110dA46B83EE6Cb5C791',
+            value: web3js.utils.toWei('0.2','ether')
+        })
+    }
     render(){
         return(
             <div>
             <h1> Transaction</h1>
             <Row>
                 <Col span={8}>
-                    <Input placeholder="Value"/>
+                    <Input type="text" onChange={this.updateInput} placeholder="Value"/>
                 </Col>
                 <Col span={8}>
                     <Button type="primary" onClick={this.handleClick}>Send</Button>
