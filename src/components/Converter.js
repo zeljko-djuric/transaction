b50c6f0e0c312d6deca18ffc64ee466ce51e73c1
Zeljko Djuric <zeljko94@gmail.com>
2018-10-21 20:46:02 +0200

    Added eth amount input and info for usd

---
+++ src/components/Converter.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+class Converter extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            ethAmount: 0,
+            ethPriceUsd: 0
+        }
+    }
+
+    componentWillReceiveProps(){
+        this.setState({
+            ethAmount: this.props.ethAmount
+        })
+    }
+
+    componentDidMount(){
+        axios.get('https://api.coinmarketcap.com/v1/ticker/ethereum/').then((data) =>{
+            console.log(data.data)
+            console.log(data.data[0].price_usd)
+            console.log('responding request')
+            this.setState({
+                ethPriceUsd: data.data[0].price_usd
+            })
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.ethAmount} = {this.state.ethPriceUsd * this.state.ethAmount}
+            </div>
+        )
+    }
+
+}
+
+
+export default Converter;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,43 +2,77 @@ import React from 'react';
 import { Input, Row, Col, Button } from 'antd';
 import 'antd/dist/antd.css';
 import Web3 from 'web3';
+import styled from 'styled-components';
+import Converter from './Converter.js';
+
+const Wrapper = styled.div`
+    padding: 4em;
+    background: papayawhip;
+`;
+
+const InputCool = styled(Input)`
+    margin-bottom: 1em !important;
+`;
 
 class MainContent extends React.Component{
 
     constructor(props){
         super(props);
         this.state={
-            adress: ''
+            address: '',
+            amount: 0
         }
     }
 
-    updateInput = (event) =>{
-        this.setState({adress : event.target.value})
+    updateAddress = (event) =>{
+        this.setState({address : event.target.value})
     }
 
+    
+    updateAmount = (event) =>{
+        this.setState({amount : event.target.value})
+        console.log(this.state.amount)
+    }
 
     handleClick = () =>{
         let web3js = new Web3(window.web3.currentProvider);
         web3js.eth.sendTransaction({
-            to: this.state.adress,
+            to: this.state.address,
             from: '0x1dC898665B762C781Ff6110dA46B83EE6Cb5C791',
-            value: web3js.utils.toWei('0.2','ether')
+            value: web3js.utils.toWei(this.state.amount.toString(),'ether')
         })
     }
+
+  
+
     render(){
         return(
-            <div>
+            <Wrapper>
             <h1> Transaction</h1>
             <Row>
-                <Col span={8}>
-                    <Input type="text" onChange={this.updateInput} placeholder="Value"/>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <InputCool type="text" onChange={this.updateAddress} placeholder="Address To"/>
                 </Col>
-                <Col span={8}>
+            </Row>
+
+            <Row>
+            <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <InputCool type="text"  onChange={this.updateAmount} placeholder="ETH Amount"/>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <Converter ethAmount={this.state.amount}/>
+                </Col>
+            </Row>
+             <Row>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
                     <Button type="primary" onClick={this.handleClick}>Send</Button>
                 </Col>
             </Row>
             
-            </div>
+            </Wrapper>
         )
     }
 }
